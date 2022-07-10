a6859fe907e6cb1a41cd02a231818c5677d8bb4c
Lijie Huang <huanglijie@outlook.com>
2022-07-10 13:32:02 +0800

    update gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: '/myrunning', // Change to `/running_page` when running on github pages
+  pathPrefix: '/myRunning', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'MyRunning',
     siteUrl: 'https://sealhuang.github.io',
